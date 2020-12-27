24ade86b38eba6098e741011532db89d80cf999d
helosshi <the24sfc@gmail.com>
2020-12-28 01:42:53 +0900

    connection is closedの問題解消

--- memo.rb
+++ memo.rb
@@ -7,27 +7,40 @@ require './connection_to_db'
 enable :method_override
 
 def reorder_memo
-  connection_to_db
-  @results = CONNECTION.exec('SELECT * FROM memos ORDER BY create_time')
+  db_connection = connection_to_db
+  @results = db_connection.exec('SELECT * FROM memos ORDER BY create_time')
+  db_connection.finish
 end
 
 def add_memo
-  CONNECTION.exec(
+  db_connection = connection_to_db
+  result = db_connection.exec(
     'INSERT INTO memos(title,details,create_time,last_edit_time) values($1,$2,now(),now())',
     [@title, @details]
   )
+  db_connection.finish
+  result
 end
 
 def check_memo
-  CONNECTION.exec('SELECT * FROM memos WHERE id=$1', [@id])
+  db_connection = connection_to_db
+  result = db_connection.exec('SELECT * FROM memos WHERE id=$1', [@id])
+  db_connection.finish
+  result
 end
 
 def update_memo
-  CONNECTION.exec('UPDATE memos SET title = $1,details = $2,last_edit_time = now() WHERE id = $3', [@title, @details, @id])
+  db_connection = connection_to_db
+  result = db_connection.exec('UPDATE memos SET title = $1,details = $2,last_edit_time = now() WHERE id = $3', [@title, @details, @id])
+  db_connection.finish
+  result
 end
 
 def delete_memo
-  CONNECTION.exec('DELETE FROM memos WHERE id = $1', [@id])
+  db_connection = connection_to_db
+  result = db_connection.exec('DELETE FROM memos WHERE id = $1', [@id])
+  db_connection.finish
+  result
 end
 
 # helpers
@@ -56,7 +69,6 @@ get '/' do
     memo_list << "<li class=list-group-item}><a href=#{"/#{result['id']}"}>#{h(result['title'])}</a></li>"
   end
   @memo_list = memo_list
-  CONNECTION.finish
   erb :index
 end
 
