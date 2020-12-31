d7c9f0eceb008e1994c0fc4bdd0aeaedb11b9034
helosshi <the24sfc@gmail.com>
2020-12-31 16:41:16 +0900

    CONNECTIONの残り削除

--- connection_to_db.rb
+++ connection_to_db.rb
@@ -1,6 +1,3 @@
-CONNECTION = PG.connect(host: 'localhost', user: 'postgres', dbname: 'fjord_memo_db')
-CONNECTION.internal_encoding = 'UTF-8'
-
 def connection_to_db
   PG.connect(host: 'localhost', user: 'postgres', dbname: 'fjord_memo_db')
 end
